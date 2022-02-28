ab0b068282f70c4bf76673d9974ebc6f893efb63
bakroli <bak.roland@hotmail.com>
2022-02-28 16:08:01 +0100

    partly

--- src/main/java/hu/nive/ujratervezes/plants/PlantManager.java
+++ src/main/java/hu/nive/ujratervezes/plants/PlantManager.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.plants;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlantManager {
 
     private final String dbUrl;
@@ -11,4 +15,29 @@ public class PlantManager {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    public List<String> getPlantsToBeWatered() {
+        List<String> plants = new ArrayList<>();
+        String sqlQuery = "SELECT DISTINCT(plant_name) FROM plants JOIN watered ON plants.plant_id = watered.plant_id WHERE watered_at < '2021-12-31' ORDER BY plant_name ASC";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl,dbUser,dbPassword)) {
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            plants = getResult(statement.executeQuery());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return plants;
+    }
+
+    private static List<String> getResult(ResultSet resultSet) throws SQLException {
+        List<String> result = new ArrayList<>();
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+        }
+        return result;
+    }
+
+
 }
